0df0daffd96ea618fcb952223ec6e7bdc0ed8aae
Reece Kelly <reecekelly2004@outlook.com>
2025-06-18 14:21:37 +1000

    Add book data class

---
+++ app/src/main/java/com/example/assignmentapp/data/Book.kt
@@ -0,0 +1,9 @@
+package com.example.assignmentapp.data
+
+data class Book(
+    val title: String,
+    val author: String,
+    val yearPublished: Int,
+    val rating: Double,
+    val review: String,
+)
