f5894aaa8bd429b957c62b42ffb15b962b6bd565
Reece Kelly <reecekelly2004@outlook.com>
2025-07-13 17:34:58 +1000

    Add temporary hardcoded ids to books within getBooks function

--- app/src/main/java/com/example/assignmentapp/data/BooksRepositoryImpl.kt
+++ app/src/main/java/com/example/assignmentapp/data/BooksRepositoryImpl.kt
@@ -3,9 +3,9 @@ package com.example.assignmentapp.data
 class BooksRepositoryImpl : BooksRepository {
     override fun getBooks(): List<Book> {
         return listOf(
-            Book("1984", "George Orwell", 1949, 250, 356, 4.5, "Great book!"),
-            Book("Dune", "Frank Herbert", 1964, 200, 512, 4.0, "Sandy book! I like the worms!"),
-            Book("Animal Farm", "George Orwell", 1945, 0, 192, 4.5, "Absolute power corrupts absolutely!"),
+            Book("0001", "1984", "George Orwell", 1949, 250, 356, 4.5, "Great book!"),
+            Book("0002", "Dune", "Frank Herbert", 1964, 200, 512, 4.0, "Sandy book! I like the worms!"),
+            Book("0003", "Animal Farm", "George Orwell", 1945, 0, 192, 4.5, "Absolute power corrupts absolutely!"),
         )
     }
 }
