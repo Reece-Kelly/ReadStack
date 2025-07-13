1a9394ac2e49e0cf4f888176d57b4dcbd307c8fe
Reece Kelly <reecekelly2004@outlook.com>
2025-07-13 17:31:13 +1000

    Add id attribute to the book data class

--- app/src/main/java/com/example/assignmentapp/data/Book.kt
+++ app/src/main/java/com/example/assignmentapp/data/Book.kt
@@ -1,11 +1,12 @@
 package com.example.assignmentapp.data
 
 data class Book(
+    val id: String,
     val title: String,
     val author: String,
     val yearPublished: Int,
     val currentPageNumber: Int,
     val totalPageNumber: Int,
-    val rating: Double,
-    val review: String,
+    val rating: Double?, // Nullable if not all books have ratings
+    val review: String?, // Nullable if not all books have reviews
 )
