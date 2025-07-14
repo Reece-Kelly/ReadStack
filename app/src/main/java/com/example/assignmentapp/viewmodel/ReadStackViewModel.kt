7a28017692a8856eb1d9046990ab9a3854227cca
Reece Kelly <reecekelly2004@outlook.com>
2025-07-14 21:39:24 +1000

    Update getBooks function for network integration

--- app/src/main/java/com/example/assignmentapp/viewmodel/ReadStackViewModel.kt
+++ app/src/main/java/com/example/assignmentapp/viewmodel/ReadStackViewModel.kt
@@ -1,20 +1,38 @@
 package com.example.assignmentapp.viewmodel
 
 import androidx.lifecycle.ViewModel
-import com.example.assignmentapp.ReadStackUiState
+import androidx.lifecycle.viewModelScope
+import com.example.assignmentapp.views.ReadStackUIState
 import com.example.assignmentapp.data.BooksRepository
+import com.example.assignmentapp.data.NetworkResult
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
-
-//class ReadStackViewModel: ViewModel() {
-//    private val _uiState = MutableStateFlow(ReadStackUiState())
-//    val uiState: StateFlow<ReadStackUiState> = _uiState.asStateFlow()
-//}
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
 class ReadStackViewModel(
     private val booksRepository: BooksRepository
 ): ViewModel() {
+    val readStackUIState = MutableStateFlow(ReadStackUIState())
 
-    fun getBooks() = booksRepository.getBooks()
+    init {
+        getBooks()
+    }
+
+    private fun getBooks() {
+        readStackUIState.value = ReadStackUIState(isLoading = true)
+        viewModelScope.launch {
+            when (val result = booksRepository.getBooks()) {
+                is NetworkResult.Success -> {
+                    readStackUIState.update {
+                        it.copy(isLoading = false, books = result.data)
+                    }
+                }
+                is NetworkResult.Error -> {
+                    readStackUIState.update {
+                        it.copy(isLoading = false, error = result.error)
+                    }
+                }
+            }
+        }
+    }
 }
