3b887db73646b87fb9b4ea4ec45611d85f7f63f2
Reece Kelly <reecekelly2004@outlook.com>
2025-07-16 16:11:48 +1000

    Add volume class for use in storing Google Book API response

---
+++ app/src/main/java/com/example/assignmentapp/data/Volume.kt
@@ -0,0 +1,43 @@
+package com.example.assignmentapp.data
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+
+@Serializable
+data class GoogleBooksApiResponse(
+    @SerialName("items")
+    val items: List<Volume>?
+)
+
+@Serializable
+data class Volume(
+    @SerialName("id")
+    val id: String,
+    @SerialName("volumeInfo")
+    val volumeInfo: VolumeInfo
+)
+
+@Serializable
+data class VolumeInfo(
+    @SerialName("title")
+    val title: String = "",
+    @SerialName("authors")
+    val authors: List<String>?,
+//    @SerialName("description")
+//    val description: String?,
+    @SerialName("publisher")
+    val publisher: String?,
+    @SerialName("publishedDate")
+    val publishedDate: String?,
+    @SerialName("imageLinks")
+    val imageLinks: ImageLinks? = null
+)
+
+@Serializable
+data class ImageLinks(
+    @SerialName("thumbnail")
+    val thumbnail: String = "",
+    @SerialName("smallThumbnail")
+    val smallThumbnail: String = ""
+)
