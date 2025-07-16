ebcaeab0d18ba767d306ebe760eb1c636dd21689
Reece Kelly <reecekelly2004@outlook.com>
2025-07-16 16:10:41 +1000

    Add volumes and volumeList attributes to the UI state class

--- app/src/main/java/com/example/assignmentapp/views/ReadStackUIState.kt
+++ app/src/main/java/com/example/assignmentapp/views/ReadStackUIState.kt
@@ -1,10 +1,14 @@
 package com.example.assignmentapp.views
 
 import com.example.assignmentapp.data.Book
+import com.example.assignmentapp.data.GoogleBooksApiResponse
+import com.example.assignmentapp.data.Volume
 
 data class ReadStackUIState (
-    val books: List<Book> = emptyList(), // Uncomment once implemented
+    val books: List<Book> = emptyList(), // Attribute to store the list of books (once converted from volumes from API)
+    val volumes: GoogleBooksApiResponse = GoogleBooksApiResponse(emptyList()),
+    val volumeList: List<Volume> = emptyList(), // Attribute to store the list of volumes that Google Book API returns
     val isLoading: Boolean = false,
     val currentSearchWord: String = "",
-    val error: String? = null,
+    val error: String? = null, // Attribute to store the error message if any
 )
