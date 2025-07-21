cad22be5dd26b5b80d0d1f26eb561b5658629492
Reece Kelly <reecekelly2004@outlook.com>
2025-07-21 22:55:04 +1000

    Add placeholder text for suggest screen

--- app/src/main/java/com/example/assignmentapp/views/SuggestScreen.kt
+++ app/src/main/java/com/example/assignmentapp/views/SuggestScreen.kt
@@ -1,23 +1,33 @@
 package com.example.assignmentapp.views
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FabPosition
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.assignmentapp.Title
 import com.example.assignmentapp.views.BottomNavBar
 import com.example.assignmentapp.data.Volume
 import com.example.assignmentapp.viewmodel.ReadStackViewModel
 import org.koin.androidx.compose.getViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun SuggestScreen(onBookClicked: (Volume) -> Unit) {
+fun SuggestScreen(
+    onBookClicked: (Volume) -> Unit,
+    navController: NavController
+) {
     val readStackViewModel: ReadStackViewModel = getViewModel()
 
     Scaffold(
@@ -29,19 +39,26 @@ fun SuggestScreen(onBookClicked: (Volume) -> Unit) {
         },
         floatingActionButtonPosition = FabPosition.End,
         bottomBar = {
-            BottomNavBar()
+            BottomNavBar(
+                modifier = Modifier,
+                navController = navController
+            )
         }
     ) { innerPadding ->
 
-
-
-        BookList(
-            viewModel = readStackViewModel,
+        Column(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(innerPadding)
-                .padding(bottom = 72.dp),
-            onBookClicked = onBookClicked // Not too sure about this line
-        )
+        ) {
+            Title(title = "ReadStack")
+
+            Text(
+                text = "The Suggest Screen! (This is a placeholder)",
+                style = MaterialTheme.typography.bodyMedium,
+                modifier = Modifier
+                    .padding(innerPadding)
+            )
+        }
     }
 }
