0704c8dbd12d903b0890c536dbb70c2c738be9b9
Reece Kelly <reecekelly2004@outlook.com>
2025-07-21 10:47:35 +1000

    Update Screens class to shorten names of new screen

--- app/src/main/java/com/example/assignmentapp/navigation/Screens.kt
+++ app/src/main/java/com/example/assignmentapp/navigation/Screens.kt
@@ -3,6 +3,6 @@ package com.example.assignmentapp.navigation
 sealed class Screens (val route: String) {
     object HomeScreen: Screens("home")
     object BookDetailsScreen: Screens("bookDetails")
-    object SearchScreen: Screens("searchScreen")
-    object SuggestScreen: Screens("suggestScreen")
+    object SearchScreen: Screens("search")
+    object SuggestScreen: Screens("suggest")
 }
