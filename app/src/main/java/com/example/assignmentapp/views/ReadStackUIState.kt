7459524d702e08ad3779a54a09d1a3a83c50608d
Reece Kelly <reecekelly2004@outlook.com>
2025-07-24 16:02:38 +1000

    Remove unnecessary attributes

--- app/src/main/java/com/example/assignmentapp/views/ReadStackUIState.kt
+++ app/src/main/java/com/example/assignmentapp/views/ReadStackUIState.kt
@@ -1,14 +1,10 @@
 package com.example.assignmentapp.views
 
-import com.example.assignmentapp.data.Book
-import com.example.assignmentapp.data.GoogleBooksApiResponse
 import com.example.assignmentapp.data.Volume
 
-data class ReadStackUIState (
-    val books: List<Book> = emptyList(), // Attribute to store the list of books (once converted from volumes from API)
-    val volumes: GoogleBooksApiResponse = GoogleBooksApiResponse(emptyList()),
-    val volumeList: List<Volume> = emptyList(), // Attribute to store the list of volumes that Google Book API returns
+data class ReadStackUIState(
+    val volumes: List<Volume> = emptyList(),
     val isLoading: Boolean = false,
     val currentSearchWord: String = "",
-    val error: String? = null, // Attribute to store the error message if any
+    val error: String? = null
 )
