458cca55e40491d1badc51df711ce542427b6633
Reece Kelly <reecekelly2004@outlook.com>
2025-07-28 13:30:38 +1000

    Add searchResults variable

--- app/src/main/java/com/example/assignmentapp/views/ReadStackUIState.kt
+++ app/src/main/java/com/example/assignmentapp/views/ReadStackUIState.kt
@@ -4,6 +4,7 @@ import com.example.assignmentapp.data.Volume
 
 data class ReadStackUIState(
     val volumes: List<Volume> = emptyList(),
+    val searchResults: List<Volume> = emptyList(),
     val isLoading: Boolean = false,
     val currentSearchWord: String = "",
     val error: String? = null
