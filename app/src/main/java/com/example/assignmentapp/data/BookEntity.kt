52c93524dd5988ac388cad5cbdb20fa67faf1b44
Reece Kelly <reecekelly2004@outlook.com>
2025-07-30 23:13:01 +1000

    Add default values for multiple attributes

--- app/src/main/java/com/example/assignmentapp/data/BookEntity.kt
+++ app/src/main/java/com/example/assignmentapp/data/BookEntity.kt
@@ -15,9 +15,9 @@ data class BookEntity(
     val thumbnail: String,
     val smallThumbnail: String,
     val status: BookStatus?,
-    val currentPageNumber: Int?,
-    val totalPageNumber: Int?,
-    val rating: Double?,
+    val currentPageNumber: Int? = 0,
+    val totalPageNumber: Int? = 0,
+    val rating: Float? = null,
     val review: String? = null,
 )
 
