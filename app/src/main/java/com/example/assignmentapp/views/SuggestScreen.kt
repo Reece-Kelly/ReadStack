0bea0b8f76f61d13cf6a477b559218884535f8c1
Reece Kelly <reecekelly2004@outlook.com>
2025-07-30 13:08:53 +1000

    Remove unnecessary imports and commented out code

--- app/src/main/java/com/example/assignmentapp/views/SuggestScreen.kt
+++ app/src/main/java/com/example/assignmentapp/views/SuggestScreen.kt
@@ -3,23 +3,15 @@ package com.example.assignmentapp.views
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FabPosition
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.assignmentapp.Title
-import com.example.assignmentapp.views.BottomNavBar
 import com.example.assignmentapp.data.Volume
-import com.example.assignmentapp.navigation.Screens
 import com.example.assignmentapp.viewmodel.ReadStackViewModel
 import org.koin.androidx.compose.getViewModel
 
@@ -33,8 +25,6 @@ fun SuggestScreen(
 
     Scaffold(
         modifier = Modifier.fillMaxSize(),
-//        floatingActionButton = { SearchFab(navController) },
-//        floatingActionButtonPosition = FabPosition.End,
         bottomBar = {
             BottomNavBar(
                 modifier = Modifier,
